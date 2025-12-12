816732a3e776e40e9f19c70548bafd5e9c186718
Emmanuel <emmytech2008@gmail.com>
2025-12-12 13:57:47 +0100

    fix(build): remove duplicate supabase key argument

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -7,7 +7,6 @@ export function createServerSupabaseClient() {
     return createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookieOptions: {
                 name: 'trove-session',
